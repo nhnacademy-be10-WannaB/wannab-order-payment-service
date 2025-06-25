fda2fe91ce264a806d839c3921829322e43d6065
{Minsooooooo} <{kinggodminsu@naver.com}>
2025-06-25 16:54:59 +0900

    fix: 주문관리로직 추가

--- src/main/java/shop/wannab/order_payment_service/OrderPaymentServiceApplication.java
+++ src/main/java/shop/wannab/order_payment_service/OrderPaymentServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableScheduling	//주문상태 <- 일정시간지나면 배송완료로 변경하기위해 추가
 @SpringBootApplication
 public class OrderPaymentServiceApplication {
 
