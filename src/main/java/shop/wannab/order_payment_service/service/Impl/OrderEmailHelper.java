0287d06fc6edcae0d92de577b89cb330adc85d36
LimJaeEun0930 <je9513666@gmail.com>
2025-06-27 16:50:48 +0900

    refactor: orderEmailHelper 리팩토링

--- src/main/java/shop/wannab/order_payment_service/service/Impl/OrderEmailHelper.java
+++ src/main/java/shop/wannab/order_payment_service/service/Impl/OrderEmailHelper.java
@@ -2,20 +2,21 @@ package shop.wannab.order_payment_service.service.Impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import shop.wannab.order_payment_service.client.UserClient;
-import shop.wannab.order_payment_service.entity.Guest;
+
 import shop.wannab.order_payment_service.entity.Order;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class OrderEmailHelper {
 
     private final EmailService emailService;
-    private final UserClient userClient;
+
 
     public void sendOrderEmail(Order order, String email, String address, String name) {
 
-
+        LocalDate deliveryWant = order.getDeliveryWant();
         String emailText = String.format("""
                 %s님, 주문이 완료되었습니다.
 
@@ -25,7 +26,7 @@ public class OrderEmailHelper {
                 ▷ 배송희망일: %s
 
                 감사합니다.
-            """, name, order.getId(), order.getTotalPrice(), address, order.getDeliveryWant());
+            """, name, order.getId(), order.getTotalPrice(), address, deliveryWant == null ? "없음" : deliveryWant);
 
         emailService.sendOrderEmail(email, "[WannaB] 주문확인서", emailText);
     }
